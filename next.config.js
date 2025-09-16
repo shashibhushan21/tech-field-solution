53b1a77dad14db0282b6cd8e4da83edbab00d922
shashibhushan21 <shashi.bkumar21@gmail.com>
2025-09-17 02:03:32 +0530

    fixed bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
@@ -37,4 +35,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
